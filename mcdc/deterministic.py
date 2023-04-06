70aa150e3ffcaf11d1864d1bb72144cc92a4841f
Charles Goodman <cegoodma@ncsu.edu>
2023-04-06 12:57:56 -0400

    Fixes to the QD solver

--- mcdc/deterministic.py
+++ mcdc/deterministic.py
@@ -25,64 +25,65 @@ def QD1D(mcdc, phi_in, J_in, Edd_in):
     nu = mcdc['materials'][0]['nu_p'][0]
     v = mcdc['materials'][0]['speed'][0]
 
-    Edd = np.zeros((Nx+2,))
-    Edd[0] = Edd_in[0]
-    Edd[1:-1] = Edd_in[:]
-    Edd[-1] = Edd_in[-1]
-
-    #Solve transport problem using QD given eddington tensors
+    Edd2 = np.zeros((Nx+2,))
+    Edd2[0] = Edd_in[0]
+    Edd2[1:-1] = Edd_in[:]
+    Edd2[-1] = Edd_in[-1]
+    
+    #solve for phi
+    phi = np.zeros((Nx+2,))
+    J = np.zeros((Nx+1,))
+                
     #build tridiagonal matrix for cell
-    a = (Edd[:-1]/dx2/(1/v/dt+sigmaT))
-    b = (Edd[1:]/dx2/(1/v/dt+sigmaT))
+    a = (Edd2[:-1]/dx2/(1/v/dt2+sigmaT))
+    b = (Edd2[1:]/dx2/(1/v/dt2+sigmaT))
     f = np.zeros((Nx+2,))
-    f[1:-1] += dx*(1/v/dt+sigmaA-nu*sigmaF)
+    f[0] = -CL
+    f[1:-1] = dx*(sigmaT-sigmaS+1/v/dt2)
+    f[-1] = CR
     d = np.zeros((Nx+2,))
-    d[1:-1] += dx*phi_in/v/dt
-
+    d[0] = JLin-CL*phiLin-J_in[0]/v/dt2/(sigmaT+1/v/dt2)
+    d[1:-1] = dx*(q+phi_in/v/dt2)+(1/v/dt2)/(sigmaT+1/v/dt2)*(J_in[:-1]-J_in[1:])
+    d[-1] = CR*phiRin-JRin+J[s,-1]/v/dt2/(sigmaT+1/v/dt2)
     #solve for scalar flux using thomas algorithm
-    aa = -a
-    bb = f
+    aa = np.copy(-a)
+    bb = np.copy(f)
     bb[:-1] += a
     bb[1:] += b
-    cc = -b
-    dd = d
+    cc = np.copy(-b)
+    dd = np.copy(d)
     #first row
     cc[0] /= bb[0]
     dd[0] /= bb[0]
     #middle rows
     for i in range(Nx):
-        cc[i+1]/= (bb[i+1]-aa[i]*cc[i])
+        cc[i+1] /= (bb[i+1]-aa[i]*cc[i])
         dd[i+1] = (dd[i+1]-aa[i]*dd[i])/(bb[i+1]-aa[i]*cc[i])
     #last row
     dd[Nx+1] = (dd[Nx+1]-aa[Nx]*dd[Nx])/(bb[Nx+1]-aa[Nx]*cc[Nx])
 
-    #solve for phi
-    phi = np.zeros((Nx+2,))
-    J = np.zeros((Nx+1,))
-
     #last row
-    phi[Nx+1]=dd[Nx+1]
+    phi_edge[-1]=dd[Nx+1]
+    phi[Nx-1]=dd[Nx]-cc[Nx]*phi_edge[-1]
     #other rows
-    for i in range(Nx+1):
-        phi[Nx-i]=dd[Nx-i]-cc[Nx-i]*phi[Nx+1-i]
+    for i in range(Nx-1):
+        phi[Nx-i-2]=dd[Nx-i-1]-cc[Nx-i-1]*phi[Nx-i-1]
+    phi_edge[0]=dd[0]-cc[0]*phi[0]
 
     #solve for current by multiplication
-    for i in range(Nx+1):
-        J[i] = (Edd[i]/dx2[i]/(1/v/dt+sigmaT))*phi[i]-(Edd[i+1]/dx2[i]/(1/v/dt+sigmaT))*phi[i+1]+J_in[i]
+    for i in range(Nx-1):
+        J[i+1] = (Edd2[i+1]*phi[i]-Edd2[i+2]*phi[i+1]+J_in[i+1]*dx2[i+1]/v/dt2)/dx2[i+1]/(1/v/dt2+sigmaT)
+    J[0] = (Edd2[0]*phi_edge[0]-Edd2[1]*phi[0]+J_in[0]*dx2[0]/v/dt2)/dx2[0]/(1/v/dt2+sigmaT)
+    J[-1] = (Edd2[-2]*phi[-1]-Edd2[-1]*phi_edge[-1]+J_in[-1]*dx2[-1]/v/dt2)/dx2[-1]/(1/v/dt2+sigmaT)
 
-    
-    res1 = np.zeros((Nx,))
-    res2 = np.zeros((Nx+1,))
-    #residuals      TODO need to add fision term
+    #residuals
     for i in range(Nx):
-        res1[i] = J[i+1]-J[i] + (sigmaA)*dx[i]*phi[i+1]
-    for i in range(Nx+1):
-        res2[i] = Edd[i+1]*phi[i+1]-Edd[i]*phi[i]+sigmaT*dx2[i]*J[i]
-    print(np.amax(res1))
-    print(np.argmax(res1))
-    print(np.amax(res2))
-    print(np.argmax(res2))
-
+        res1[i] = J[i+1]-J[i] + (1/v/dt2+(sigmaT-sigmaS))*dx[i]*phi[i]-dx[i]/v/dt2*phi_in[i]-q*dx[i]
+    
+    res2[0] = Edd2[1]*phi[0]-Edd2[0]*phi_in[0]+sigmaT*dx2[0]*J[0]+dx2[0]/v/dt2*(J[0]-J_in[0])
+    res2[-1] = Edd2[-1]*phi_in[-1]-Edd2[-2]*phi[-1]+sigmaT*dx2[-1]*J[-1]+dx2[-1]/v/dt2*(J[-1]-J_in[-1])
+    for i in range(Nx-1):
+        res2[i+1] = Edd2[i+2]*phi[i+1]-Edd2[i+1]*phi[i]+sigmaT*dx2[i+1]*J[i+1]+dx2[i+1]/v/dt2*(J[i+1]-J_in[i+1])
 
     return phi, J
 
--- mcdc/kernel.py
+++ mcdc/kernel.py
@@ -2305,8 +2305,6 @@ def get_weight_window(P, mcdc):
     
 @njit
 def weight_window(P, mcdc):
-<<<<<<< HEAD
-=======
 #    c = 1.1
 #    i = complex(0,1)
 #    x = P['x']
@@ -2320,7 +2318,6 @@ def weight_window(P, mcdc):
 #        integral = quad(integrand,0.0,np.pi,args=(eta,t))[0]
 #        w_target = np.e**-t/2/t*(1+c*t/4/np.pi*(1-eta**2)*integral)
 #    if (True):
->>>>>>> 76ddeee58c8f89c91dbb76457a8919e9ef0b74a1
     # Get indices
     t, x, y, z, outside = mesh_get_index(P, mcdc['technique']['ww_mesh'])
 
